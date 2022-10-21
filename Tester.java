ba41adf973fad69371c1832d942f2e35387a110a
MariamAsbhi <111119638+MariamAsbhi@users.noreply.github.com>
2022-10-21 23:30:44 +0300

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Tester {
+    public static void main(String[] args) {
+        BasicAccount bb=new BasicAccount();
+        bb.deposits(100);
+        System.out.println(bb.getBalance());
+        bb.withdraws(101);
+        System.out.println(bb.getBalance());
+//        FillInQuestion f=new FillInQuestion();
+//        f.setText("MY NAME IS _MARIAM_");
+//        f.display();
+//        System.out.println(f.getTextWithAnswer());
+//         Questions q=new Questions();
+//         q.setText("what is your name?");
+//         q.setAnswer("MARIAM");
+//         q.display();
+//        System.out.println(q.check_answer("MARIAM "));
+//
+//        Student s=new Student("AHMED","2020","NOTHING");
+//        System.out.println(s.toString());
+//        Instructor i=new Instructor("ELAF","2015",10000);
+//        System.out.println(i.toString());
+//
+//
+//        BetterRectangle b=new BetterRectangle(0,0,1,2);
+//        System.out.println(b.getPerimeter());
+//        System.out.println(b.getArea());
+    }
+}
